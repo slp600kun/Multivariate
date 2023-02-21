a37f9155599612e62a979a88bb751758d5410a07
slp600kun <kawaguchi.h.ad@m.titech.ac.jp>
2023-02-21 11:22:55 +0900

    #1 create conert3 ipynb

--- data/sensordata.py
+++ data/sensordata.py
@@ -3,7 +3,7 @@
 """
 Created on Wed Apr 21 17:10:08 2021
 
-@author: shakeel
+@author: kawaguchi
 """
 import scipy.io as sc
 import numpy as np
@@ -20,9 +20,9 @@ def convert_mat_to_list(mat_dir,feature_type):
     """
 
     mat_files = os.listdir(mat_dir)
-    aud_a=[]
-    aud_b=[]
-    aud_y=[]
+    mat2_a=[]
+    mat2_b=[]
+    mat2_y=[]
     for i in range(len(mat_files)):
             a_path = mat_files[i]
             temp1 = sc.loadmat(mat_dir + a_path)
@@ -31,11 +31,31 @@ def convert_mat_to_list(mat_dir,feature_type):
                 b_path = mat_files[j]
                 temp2 = sc.loadmat(mat_dir + b_path)
                 feat2 = np.array(temp2[feature_type]).reshape(999,50)
-                aud_a.append(feat1)
-                aud_b.append(feat2)
+                mat2_a.append(feat1)
+                mat2_b.append(feat2)
                 if(a_path[0]==b_path[0]):
-                    aud_y.append(ord(a_path[0])-48)
+                    mat2_y.append(ord(a_path[0])-48)
                 else:
-                    aud_y.append(-1)
+                    mat2_y.append(-1)
 
-    return aud_a,aud_b,aud_y
+    return mat2_a,mat2_b,mat2_y
+
+def data_generate(mat1_dir,mat1_feat,mat2_dir,mat2_feat):
+    """
+    
+    """
+
+
+    mat1_a,mat1_b,mat1_y = convert_mat_to_list(mat1_dir,mat1_feat)
+    mat2_a,mat2_b,mat2_y = convert_mat_to_list(mat2_dir,mat2_feat)
+    label=[]
+
+    for i in range(len(mat2_a)):
+        if(mat1_y[i] == -1 or mat2_y[i] == -1):
+            label.append(0)
+        elif(mat1_y[i] == mat2_y[i]):
+            label.append(1)
+        else:
+            label.append(0)
+
+    return mat1_a,mat2_a,mat1_b,mat2_b,label
